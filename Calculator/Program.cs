8174def60d5146a4dce4e820f22d3e9eb6586c10
tomasvaro <tomas.varo63@gmail.com>
2021-09-21 18:24:01 +0200

    Added "Choose alternatives", enter first and second number.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,14 +6,49 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Calculator");
-            Console.WriteLine("Välj ett av alternativen:");
-            Console.WriteLine("1 - Addition");
-            Console.WriteLine("2 - Subtraktion");
-            Console.WriteLine("3 - Multiplikation");
-            Console.WriteLine("4 - Divition");
-
-
+            bool running = true;
+            Console.WriteLine("Welcome to Calculator\n");
+            while (running)
+            {
+                Console.WriteLine("Choose one of the alternatives:\n1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division\n5 - End application");
+                try
+                {
+                    int userInput = int.Parse(Console.ReadKey().KeyChar.ToString());
+                    Console.WriteLine();
+                    if (userInput > 0 && userInput < 6)
+                    {
+                        bool isNumber = false;
+                        while (!isNumber)
+                        {
+                            try
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Enter the first number:");
+                                double firstNumber = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("\nEnter the second number:");
+                                double secondNumber = Convert.ToDouble(Console.ReadLine());
+                                isNumber = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("You must enter a digit or decimal number! Try again.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You must enter a digit between 1-5! Try again.\n");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nYou must enter a digit! Try again.\n");
+                }
+            }
+        }
+        static int GetNumberFromUser()
+        {
+            return 0;
         }
     }
 }
