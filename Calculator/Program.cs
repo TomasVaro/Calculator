93bab22815daf9afe934b9794546e3dc2ad9011c
tomasvaro <tomas.varo63@gmail.com>
2021-09-21 09:56:33 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Calculator");
+            Console.WriteLine("Välj ett av alternativen:");
+            Console.WriteLine("1 - Addition");
+            Console.WriteLine("2 - Subtraktion");
+            Console.WriteLine("3 - Multiplikation");
+            Console.WriteLine("4 - Divition");
+
+
+        }
+    }
+}
